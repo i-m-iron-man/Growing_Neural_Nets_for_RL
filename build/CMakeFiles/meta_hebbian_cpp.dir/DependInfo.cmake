3760948d7d699985eb9f1e1e6f6faba81baa19ce
i-m-iron-man <sidchat.sc@gmail.com>
2022-04-23 00:22:30 +0200

    second commit

---
+++ build/CMakeFiles/meta_hebbian_cpp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/Bond.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/Bond.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/Growing_Machines.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/Growing_Machines.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/Net.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/Net.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/Neuron.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/Neuron.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/env.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/env.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/help.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/help.cpp.o"
+  "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/main.cpp" "/home/sid/source/Growing_Nets/Source/meta_hebbian_cpp/build/CMakeFiles/meta_hebbian_cpp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/sid/library/mujoco/mujoco210/include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/meta_hebbian_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meta_hebbian_cpp.dir/Bond.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/Growing_Machines.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/Net.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/Neuron.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/env.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/help.cpp.o"
+  "CMakeFiles/meta_hebbian_cpp.dir/main.cpp.o"
+  "meta_hebbian_cpp"
+  "meta_hebbian_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meta_hebbian_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
